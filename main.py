61ccc7a1a0f8f006cc0d16be3b580a231b37df5d
Kashish Singhal <kashishsinghal@0319gmail.com>
2026-08-30 19:06:26 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import random
+import string
+
+length = int(input("Enter the length of password: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
